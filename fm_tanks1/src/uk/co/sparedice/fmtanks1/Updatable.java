9ebf216b98c71c7d6e68156a71364d8c23fcc850
Michael Clapham <michaeljclapham@gmail.com>
2013-07-14 08:03:16 +0100

    added tank class

--- fm_tanks1/src/uk/co/sparedice/fmtanks1/Updatable.java
+++ fm_tanks1/src/uk/co/sparedice/fmtanks1/Updatable.java
@@ -1,3 +1,8 @@
+/**
+ * Updatable is a generic class that should be updated when added to the game world.
+ * It is also rendered onto the game world.
+ */
+
 package uk.co.sparedice.fmtanks1;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
