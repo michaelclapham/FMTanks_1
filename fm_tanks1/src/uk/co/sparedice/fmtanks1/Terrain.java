f6fd3e16d64b39cfafa5e8f19dadf7d84db1fa5a
Michael I.J. Clapham <michaeljclapham@gmail.com>
2013-07-15 22:48:43 +0100

    added extra textures

--- fm_tanks1/src/uk/co/sparedice/fmtanks1/Terrain.java
+++ fm_tanks1/src/uk/co/sparedice/fmtanks1/Terrain.java
@@ -1,30 +1,63 @@
 package uk.co.sparedice.fmtanks1;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.TextureData;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Terrain {
 	
-	private short tileData[][];
+	private int tileData[][];
 	private int width = 40;
 	private int height = 40;
-	private int tileSize = 20;
+	private int tileSize = 80;
+	
+	//Basic Textures
 	private Texture grassTexture;
+	private int grassColor;
+	private Texture desertTexture;
+	private int desertColor;
 	
 	public Terrain(){
-		tileData = new short[width][height];
+		tileData = new int[width][height];
 		grassTexture = new Texture(Gdx.files.internal("grass1.png"));
+		desertTexture = new Texture(Gdx.files.internal("desert1.png"));
+		tileData[1][1] = 1;
+		tileData[1][2] = 1;
+		tileData[2][3] = 1;
+	}
+	
+	public Terrain(String pathToBitmap){
+		loadFromBitmap(pathToBitmap);
 	}
 	
 	public void drawTerrain(SpriteBatch batch){
 		for(int i = 0; i < width; i++){
 			for(int j = 0; j < height; j++){
 				if(tileData[i][j] == 0){
+					batch.draw(desertTexture, i*tileSize, j*tileSize, tileSize, tileSize);
+				}
+				if(tileData[i][j] == 1){
 					batch.draw(grassTexture, i*tileSize, j*tileSize, tileSize, tileSize);
 				}
 			}
 		}
 	}
+	
+	public void loadFromBitmap(String path){
+		Texture tex = new Texture (Gdx.files.internal(path));
+		width = tex.getWidth();
+		height = tex.getHeight();
+		tileData = new int[width][height];
+		Pixmap data = tex.getTextureData().consumePixmap();
+		for(int i = 0; i < width; i++){
+			for(int j = 0; j < height; j++){
+				tileData[i][j] = data.getPixel(i,j);
+			}
+		}
+	}
+	
+	
 
 }
